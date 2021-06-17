0027432d604ea27638f5bf538b64a84fad9ff0e6
Pawel <01133373@pw.edu.pl>
2021-06-17 22:35:01 +0200

    move to new folder

--- db.js
+++
@@ -1,8 +0,0 @@
-const Pool = require("pg").Pool;
-const connectionString = 'postgres://euodhzgk:3MjICPsnmRRdaNLycPLyznfdGbjce6Wy@tai.db.elephantsql.com/euodhzgk'
-
-const pool = new Pool ({
-    connectionString,
-});
-
-module.exports = pool;
