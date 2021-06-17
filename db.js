11108910777ab560f681a625ca75275db74ac0a6
Pawel <01133373@pw.edu.pl>
2021-06-17 17:30:43 +0200

    Add remote database

--- db.js
+++ db.js
@@ -1,11 +1,8 @@
 const Pool = require("pg").Pool;
+const connectionString = 'postgres://euodhzgk:3MjICPsnmRRdaNLycPLyznfdGbjce6Wy@tai.db.elephantsql.com/euodhzgk'
 
 const pool = new Pool ({
-    user: "postgres",
-    password: "root",
-    host: "localhost",
-    port: 5432,
-    database: "postgres"
+    connectionString,
 });
 
 module.exports = pool;
